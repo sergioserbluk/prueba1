643e09cf5be993cab7e5adf3e951b4e956268824
sergioserbluk <sergiodserbluk@gmail.com>
2023-08-15 21:19:38 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>con code igniter</title>
+</head>
+<body>
+    <header>
+        <nav>
+
+        </nav>
+        <h1>con code igniter</h1>
+    </header>
+    <aside>
+
+    </aside>
+    <section>
+        <article>
+
+        </article>
+    </section>
+    <footer>
+        
+    </footer>
+</body>
+</html>
