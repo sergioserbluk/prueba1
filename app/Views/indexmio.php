1059f0e0c44eefc08ed1dfec01775009738bb5f7
sergioserbluk <sergiodserbluk@gmail.com>
2023-08-21 22:34:22 -0300

    21b del 8

---
+++ app/Views/indexmio.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>con code igniter</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="app/views/mipag.html">mipag</a>
+        </nav>
+        <h1>con code igniter uno</h1>
+    </header>
+    <aside>
+
+    </aside>
+    <section>
+        <article>
+
+        </article>
+    </section>
+    <footer>
+        
+    </footer>
+</body>
+</html>
